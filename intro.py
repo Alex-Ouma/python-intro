6a5c08853230ebbe7ce7c3eda32c8651c95d7ec4
Alex_Ouma <alex2019ouma@gmail.com>
2025-07-29 20:16:28 +0300

    python intro

---
+++ intro.py
@@ -0,0 +1,34 @@
+# variable it should be one word, use underscore to separate two words and Data type eg boolean,float, strings Interger
+#PLP ="Academy"
+# Student_Name = "Alex Barasa"
+# Student_ID = 1010
+# Student_Age = 25
+# Fees_Clearance = True
+#Output
+# print(Student_Name, Student_ID, Student_Age, Fees_Clearance)
+# Creating a list
+languages = ["Python", "Java", "Javascript", 23]
+print(languages)
+
+# Tuple
+products = ('XBox', 499.99, "Habibi", 23)
+print(products)
+
+# Sets are unique data, uses culbraket
+# Set_A = {1, 2, 3, 4, 5}
+# Set_B = {4, 5, 6, 7, 8}
+# print(Set_A)
+# print(Set_B)
+
+# # Dictionaries
+# Student_info = {"Name":"Alex Ouma", "ID": "1010", }
+# print(Student_info)
+# capital_city = {"Kenya": "Nairobi", "Nigeria": "Lagos"}
+# print(capital_city)
+
+# Logical operators
+a = 10
+b = 20
+c = 30
+print(a < b)
+print(a > b and c > b)
